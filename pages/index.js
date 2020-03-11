e2b747a95eec76ca92a3e30d31336a4c51836748
Bruno Bertolini <dev@brunobertolini.com>
2020-03-11 06:39:20 -0300

    feat: add tailwind config

--- pages/index.js
+++ pages/index.js
@@ -1,202 +1,60 @@
 import Head from 'next/head'
+import '../styles/main.css'
+
+const Logo = () => (
+  <>
+    <img src="/logo.png" width={150} className="p-2 hidden md:inline-block" />
+    <img src="/codar.png" width={50} className="p-2 md:hidden" />
+  </>
+)
+
+const Menu = () => (
+  <div className="p-2 flex items-center">
+    <a href="http://github.com/codarme" target="_blank">
+      github
+    </a>
+
+    <a href="http://instagram.com/codar.me" target="_blank">
+      instagram
+    </a>
+
+    <a href="http://youtube.com/codarme" target="_blank">
+      youtube
+    </a>
+  </div>
+)
+
+const Banner = () => (
+  <div className="container max-w-5xl m-auto p-6 flex-1 flex flex-col justify-center items-center sm:items-start text-center sm:text-left">
+    <span className="text-gray">Criamos a codar.me para te ajudar a</span>
+    <div className="uppdercase text-4xl font-extrabold leading-snug">
+      Desenvoler <span className="text-green">software</span> de verdade
+    </div>
+    <span className="text-gray">
+      e se tornar um desenvolvedor acima da média
+    </span>
+  </div>
+)
+
+const Header = () => (
+  <div className="p-2 container max-w-5xl m-auto flex justify-between items-center">
+    <Logo />
+    <Menu />
+  </div>
+)
 
 const Home = () => (
-  <div className="container">
+  <div
+    className="flex flex-col flex-1"
+    style={{ background: 'url(/bg.png) no-repeat center/cover' }}
+  >
     <Head>
-      <title>Create Next App</title>
+      <title>Codar.me - Tailwind + NextJS</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
+    <Header />
+    <Banner />
   </div>
 )
 
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gray: '#c7d0d8',
+        jungle: '#1a1d28',
+        gunmetal: '#2e3142',
+        green: '#43bc70',
+        red: '#F25F5C',
+      },
+      textColor: {
+        primary: '#FFFFFF',
+        secondary: '#c7d0d8',
+      },
+      container: {
+        center: true,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
